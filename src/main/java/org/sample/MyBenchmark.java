fb5f7a0c2f28a35f87158064c69ebe54e69fd002
Serkan Özel <serkan.ozel@hazelcast.com>
2022-12-30 18:05:00 +0300

    Change timeunit

--- src/main/java/org/sample/MyBenchmark.java
+++ src/main/java/org/sample/MyBenchmark.java
@@ -3,12 +3,15 @@ package org.sample;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class MyBenchmark {
     @Benchmark
     @BenchmarkMode(Mode.SampleTime)
